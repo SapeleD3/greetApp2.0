e69e223473ed7927dda1e4502f3caca01e38e205
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-16 23:52:00 +0100

    set app icon

--- app/src/main/java/com/example/greetapp/HomeActivity.java
+++ app/src/main/java/com/example/greetapp/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.example.greetapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +16,8 @@ public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +34,7 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(toMain);
             }
         });
+
+
     }
 }
